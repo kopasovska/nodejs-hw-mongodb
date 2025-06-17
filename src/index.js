a99a924dddaea94516e91bfa98dc124091d251f0
kopasovska <v.kopasovska@gmail.com>
2025-06-17 07:25:54 +0200

    initial settings

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.use('', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
