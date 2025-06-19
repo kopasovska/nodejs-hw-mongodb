3710c82e15d2edd3528d3ffeb10c8774b4c75b9a
kopasovska <v.kopasovska@gmail.com>
2025-06-19 18:55:24 +0200

    finish homework

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
-import express from 'express';
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
 
-const app = express();
-const PORT = 3000;
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
 
-app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
-
-app.use('', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
