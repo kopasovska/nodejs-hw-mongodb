f0f7fb06fd089ff425cc554175839dcea7734bcb
kopasovska <v.kopasovska@gmail.com>
2025-06-21 13:13:07 +0200

    Start hw3-crud, refactore error handlers

---
+++ src/controllers/contacts.js
@@ -0,0 +1,27 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Student not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found a contact with id ${contactId}!`,
+    data: contact,
+  });
+};
