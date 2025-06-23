0ccf87bea8f851a1e6ff4fba5a06c1449ca0e200
kopasovska <v.kopasovska@gmail.com>
2025-06-23 15:07:12 +0200

    Add post request

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import {
+  createContactController,
   getContactByIdController,
   getContactsController,
 } from '../controllers/contacts.js';
@@ -9,5 +10,6 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
 
 export default router;
